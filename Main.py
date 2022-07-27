09bac7a868a7d6e0d8c4c305fb47d5e7c17c358b
URK21CS1064 <109951975+URK21CS1064@users.noreply.github.com>
2022-07-27 19:14:06 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -23,3 +23,12 @@ class Stack:
     def status(self):
         for i in self.items:
             print(i)
+size, queries = map(int, input().rstrip().split())
+stack = Stack(size)
+for line in range(queries):
+    values = list(map(int, input().rstrip().split()))
+    if values[0] == 1:
+        stack.push(values[1])
+    elif values[0] == 2:
+    stack.pop()
+stack.status()
